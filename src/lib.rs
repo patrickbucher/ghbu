089ffdb29349bcd39c625b059d3086c1f2d92bed
Patrick Bucher <patrick.bucher@mailbox.org>
2024-04-23 08:26:52 +0200

    refactoring; extract name and ssh_url from payload

---
+++ src/lib.rs
@@ -0,0 +1,48 @@
+use reqwest::blocking::Client;
+use serde_json::Value;
+use std::collections::HashMap;
+
+pub fn fetch_repo_ssh_urls_by_name(github_token: String) -> HashMap<String, String> {
+    let mut ssh_urls: HashMap<String, String> = HashMap::new();
+    let client = Client::new();
+    let url = "https://api.github.com/user/repos";
+    let mut page = 1;
+    loop {
+        let req = client
+            .get(url)
+            .bearer_auth(&github_token)
+            .header("Accept", "application/json")
+            .header("User-Agent", "reqwest")
+            .query(&[
+                ("affiliation", "owner"),
+                ("per_page", "20"),
+                ("page", &page.to_string()),
+            ]);
+        match req.send() {
+            Ok(res) => {
+                let payload = res.json::<serde_json::Value>();
+                match payload {
+                    Ok(payload) => match payload {
+                        Value::Array(arr) => {
+                            if arr.len() < 1 {
+                                break;
+                            }
+                            for repo in arr {
+                                if let (Some(Value::String(name)), Some(Value::String(ssh_url))) =
+                                    (repo.get("name"), repo.get("ssh_url"))
+                                {
+                                    ssh_urls.insert(name.to_string(), ssh_url.to_string());
+                                }
+                            }
+                        }
+                        _ => eprintln!("mismatch"),
+                    },
+                    _ => eprintln!("oh noes"),
+                }
+            }
+            _ => break,
+        }
+        page += 1;
+    }
+    ssh_urls
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,4 @@
 use clap::Parser;
-use reqwest::blocking::Client;
-use serde_json::Value;
 use std::{env, process};
 
 const TOKEN_ENVVAR: &str = "GITHUB_TOKEN";
@@ -12,14 +10,9 @@ struct Args {
     /// Output Directory
     #[arg(short, long)]
     to: String,
-
-    /// Accounts to backup
-    #[arg()]
-    accounts: Vec<String>,
 }
 
 fn main() {
-    // get environment variable
     let github_token = match env::vars()
         .filter(|(k, _)| k == TOKEN_ENVVAR)
         .map(|(_, v)| v)
@@ -31,51 +24,9 @@ fn main() {
             process::exit(1);
         }
     };
-
-    // parse arguments
     let args = Args::parse();
-    println!("backup accounts {:?}", args.accounts);
 
-    println!("token: {}, backup to: {}", github_token, args.to);
-
-    // do a request
-    let mut ssh_urls: Vec<String> = Vec::new();
-    let client = Client::new();
-    let url = "https://api.github.com/user/repos";
-    let mut page = 1;
-    loop {
-        let req = client
-            .get(url)
-            .bearer_auth(&github_token)
-            .header("Accept", "application/json")
-            .header("User-Agent", "reqwest")
-            .query(&[
-                ("affiliation", "owner"),
-                ("per_page", "20"),
-                ("page", &page.to_string()),
-            ]);
-        match req.send() {
-            Ok(res) => {
-                let payload = res.json::<serde_json::Value>();
-                if let Ok(Value::Array(arr)) = payload {
-                    if arr.len() < 1 {
-                        break;
-                    }
-                    for e in arr {
-                        if let Some(Value::String(ssh_url)) = e.get("ssh_url") {
-                            ssh_urls.push(ssh_url.to_string());
-                        }
-                    }
-                }
-            }
-            Err(err) => {
-                println!("{err}");
-                break;
-            }
-        }
-        page += 1;
-    }
-    for ssh_url in ssh_urls {
-        println!("{ssh_url}");
+    for (name, url) in ghbu::fetch_repo_ssh_urls_by_name(github_token) {
+        println!("{name:40} {url}");
     }
 }
